ad82a74adaf44ad5abc771bd6fb46648f0f129fb
productdevbook <hi@productdevbook.com>
2024-04-03 15:24:47 +0300

    chore: update dependencies and file paths

--- tests/vue/example.test.ts
+++ tests/vue/example.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
-import Button from '~/vue/src/App.vue'
+import Button from '../../playground-vue/src/App.vue'
 
 describe('button', () => {
   it('alert render', () => {
